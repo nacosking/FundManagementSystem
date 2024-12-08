952cce782b830e1793a7241485443356d22419fe
nacosking <teohyihern28@gmail.com>
2024-12-08 19:30:03 +0800

    create New fund thruough forms (Done)

--- api.py
+++ api.py
@@ -12,6 +12,8 @@ class FundAPI:
         self.setup_routes()
 
     def setup_routes(self):
+
+        # task 1: Endpoint to retrieve a list of all funds  
         @self.app.route('/funds', methods=['GET'])
         def get_all_funds():
             """
@@ -24,18 +26,42 @@ class FundAPI:
             
             except Exception as e:
                 return f"<p>Error: {str(e)}</p>"
+            
 
+        # task 2 : Endpoint to create a new fund  
         @self.app.route('/funds', methods=['POST'])
         def create_fund():
             """
             Create a new equity fund.
             """
             try:
-                data = request.get_json()
+                if request.is_json:
+                    data = request.get_json()
+                else:
+                    # Handle form submissions
+                    data = request.form.to_dict()
                 self.equity_manager.create_fund(data)
-                return jsonify({"message": "Equity fund created successfully."}), 201
+                return """
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Success</title>
+                </head>
+                <body>
+                    <h1>Equity fund created successfully.</h1>
+                    <a href="/funds">Back to Funds List</a>
+                </body>
+                </html>
+                """
             except Exception as e:
                 return jsonify({"error": str(e)}), 400
+            
+        
+        @self.app.route('/addfunds',  methods=['GET'])
+        def AddNewFund():
+            return render_template('AddNewFunds.html')
 
         @self.app.route('/')
         def HomaPage():
